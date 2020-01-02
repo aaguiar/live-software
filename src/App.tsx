f09624dbdc6d16c8a810ff8ef7c2df1fba683be7
José Pedro <jose.monteiro@feedzai.com>
2020-01-02 14:00:18 +0000

    Page to select project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import { useHistory } from "react-router-dom";
 
-import { Select } from "evergreen-ui";
+import { Button, SelectField } from "evergreen-ui";
 
 const App: React.FC = () => {
+  // Declare a new state variable, which we'll call "count"
+  const [projectId, setProjectId] = useState("1");
+
   let history = useHistory();
 
+  let handleSelectProject = (value: string) => {
+    setProjectId(value);
+  }
+
+  let pushToCanvasPage = () => {
+    console.log("Go");
+    history.push(`/canvas/${projectId}`);
+  }
+
   return (
-    <div>
+    <div id="slct-proj">
       <div>
-        <Select onChange={event => alert(event.target.value)}>
-          <option value="foo" selected>1</option>
-          <option value="bar">2</option>
-        </Select>
+        <SelectField
+          value={projectId}
+          width={"15%"}
+          label="Select project:"
+          onChange={(event: { target: { value: string; }; }) => handleSelectProject(event.target.value)}>
+          <option value="1">1</option>
+          <option value="2">2</option>
+        </SelectField>
       </div>
-      <button type="button" onClick={() => history.push("/canvas/1")}>
-        Go to Project
-    </button>
+      <div>
+      <Button
+        appearance="default"
+        iconBefore="circle-arrow-down"
+        onClick={() => pushToCanvasPage()}>
+        Go to Project!
+      </Button>
+      </div>
     </div>
   );
 }
